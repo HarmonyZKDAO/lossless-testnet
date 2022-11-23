7838a47f4db257084e8049ead8d757834299a890
0xlws <mailto.0xlws@gmail.com>
2022-11-23 18:01:01 +0100

    update contracts.json

--- scripts/generateContractList.js
+++ scripts/generateContractList.js
@@ -1,6 +1,6 @@
-const fs = require("fs");
+const fs = require('fs');
 
-const harmonyDeployments = `${__dirname}/../deployments/harmony`;
+const harmonyDeployments = `${__dirname}/../deployments/harmonyTestnet`;
 
 const networkDeploymentPaths = [harmonyDeployments];
 
@@ -11,7 +11,7 @@ const CURRENT_VERSION = {
 };
 
 const contractList = {
-  name: "Testnet Linked Prize Pool",
+  name: 'Testnet Linked Prize Pool',
   version: CURRENT_VERSION,
   tags: {},
   contracts: [],
@@ -32,32 +32,21 @@ const formatContract = (chainId, contractName, deploymentBlob) => {
 networkDeploymentPaths.forEach((networkDeploymentPath) => {
   const contractDeploymentPaths = fs
     .readdirSync(networkDeploymentPath)
-    .filter((path) => path.endsWith(".json"));
-  const chainId = Number(
-    fs.readFileSync(`${networkDeploymentPath}/.chainId`, "utf8")
-  );
+    .filter((path) => path.endsWith('.json'));
+  const chainId = Number(fs.readFileSync(`${networkDeploymentPath}/.chainId`, 'utf8'));
 
   contractDeploymentPaths.forEach((contractDeploymentFileName) => {
-    const contractName = contractDeploymentFileName.split(".")[0];
+    const contractName = contractDeploymentFileName.split('.')[0];
     const contractDeployment = JSON.parse(
-      fs.readFileSync(
-        `${networkDeploymentPath}/${contractDeploymentFileName}`,
-        "utf8"
-      )
+      fs.readFileSync(`${networkDeploymentPath}/${contractDeploymentFileName}`, 'utf8'),
     );
-    contractList.contracts.push(
-      formatContract(chainId, contractName, contractDeployment)
-    );
+    contractList.contracts.push(formatContract(chainId, contractName, contractDeployment));
   });
 });
 
-fs.writeFile(
-  `${__dirname}/../contracts.json`,
-  JSON.stringify(contractList),
-  (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+fs.writeFile(`${__dirname}/../contracts.json`, JSON.stringify(contractList), (err) => {
+  if (err) {
+    console.error(err);
+    return;
   }
-);
+});
